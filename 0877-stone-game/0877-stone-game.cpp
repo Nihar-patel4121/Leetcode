5d1f566266c45a5a5f7d35cd2b787713cbad05a9
Nihar-patel4121 <niharpatel14my@gmail.com>
2026-08-02 07:56:41 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.05%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,7 @@
+// iT is guranteed that the alice will win as she tries to pick up the largest number(from either from front or back) .
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
